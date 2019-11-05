1f8adb9455a122226dcaf8b7a57dc7c1b3081c46
Helsinki Hinku <helsinkihinku@Helsinki-iMac.local>
2019-11-05 14:54:05 +0200

    Wordpress Development notes added

---
+++ WordPress/Theming/global-template.php
@@ -0,0 +1,16 @@
+<!-- It’s a good idea to choose a name that describes what the template does 
+as the name is visible to WordPress users when they are editing the page.  -->
+
+<?php /* Template Name: Example Template */ ?>
+
+<!-- This example creates a page template called "Full Width Page"--> 
+
+<?php
+/**
+* Template Name: Full Width Page
+*
+* @package WordPress
+* @subpackage Twenty_Fourteen
+* @since Twenty Fourteen 1.0
+*/
+
